e1f55545ae49e82a56521104972b92063455faae
Eridanus Sora <haolinaihui@Gmail.com>
2018-04-16 20:31:50 +0800

    add webpack

---
+++ build/webpack.config.js
@@ -0,0 +1,63 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const generateHTMLPluginConf = (input) => {
+    const result = [];
+    for (const file of Object.keys(input)) {
+        result.push(new HtmlWebpackPlugin({
+            filename: path.resolve(__dirname, `../dist/${file}/index.html`),
+            template: path.resolve(__dirname, '../src/index.html'),
+        }))
+    }
+    return result;
+}
+
+const generateEntries = (input) => {
+    const result = {};
+    for (const file of Object.keys(input)) {
+        result[input[file]['output']] = path.resolve(__dirname, `../src/${input[file]['input']}`);
+    }
+    return result;
+}
+
+const generateConfig = (input) => {
+    return {
+        mode: 'production',
+        entry: generateEntries(input),
+        resolve: {
+            extensions: ['.js', '.vue']
+        },
+        plugins: [
+            new CleanWebpackPlugin([path.resolve(__dirname, '../dist')], {
+                root: path.resolve(__dirname, '../')
+            }),
+            new ExtractTextPlugin("[name].css"),
+            ...generateHTMLPluginConf(input)
+        ],
+        module: {
+            rules: [{
+                test: /\.vue$/, 
+                loader: 'vue-loader',
+                options: {
+                    extractCSS: true
+                }
+            }]
+        },
+        output: {
+            filename: '[name].js',
+            path: path.resolve(__dirname, `../dist/`),
+        }
+    }
+}
+
+module.exports = [
+    generateConfig({
+        'background': {
+            input: 'background/index.js',
+            output: 'background/bundle'
+        }
+    })
+]
---
+++ src/background/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <h2>{{ msg }}</h2>
+</template>
+<style>
+h2 {
+    color: red;
+}
+</style>
+<script>
+export default {
+    data() {
+        return {
+            msg: 'xxx'
+        }
+    }
+}
+</script>
