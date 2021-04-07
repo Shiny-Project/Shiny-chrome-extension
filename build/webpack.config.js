65f1d32f6176be75b61c88464eafddc47cc1ec90
Eridanus Sora <sora@sound.moe>
2021-04-07 10:39:44 +0800

    udpate webpack config

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,66 +1,88 @@
-const webpack = require('webpack');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const WriteFilePlugin = require("write-file-webpack-plugin");
 
 const generateHTMLPluginConf = (input) => {
     const result = [];
     for (const file of Object.keys(input)) {
-        result.push(new HtmlWebpackPlugin({
-            filename: path.resolve(__dirname, `../dist/${file}/index.html`),
-            template: path.resolve(__dirname, '../src/index.html'),
-            chunks: ['vendors', input[file]['output']]
-        }))
+        result.push(
+            new HtmlWebpackPlugin({
+                filename: path.resolve(__dirname, `../dist/${file}/index.html`),
+                template: path.resolve(__dirname, "../src/index.html"),
+                chunks: ["vendors", input[file]["output"]],
+            })
+        );
     }
     return result;
-}
+};
 
 const generateEntries = (input) => {
     const result = {};
     for (const file of Object.keys(input)) {
-        result[input[file]['output']] = path.resolve(__dirname, `../src/${input[file]['input']}`);
+        result[input[file]["output"]] = path.resolve(
+            __dirname,
+            `../src/${input[file]["input"]}`
+        );
     }
     return result;
-}
+};
 
 const generateConfig = (input) => {
     return {
-        mode: 'production',
+        mode: "production",
         entry: generateEntries(input),
         resolve: {
-            extensions: ['.js', '.vue']
+            extensions: [".js", ".vue"],
         },
         plugins: [
-            new CleanWebpackPlugin([path.resolve(__dirname, '../dist')], {
-                root: path.resolve(__dirname, '../')
+            new WriteFilePlugin(),
+            new CleanWebpackPlugin({
+                verbose: true,
             }),
-            new CopyWebpackPlugin([{
-                from: path.resolve(__dirname, '../assets/**/*'), to: path.resolve(__dirname, '../dist/'),
-            }, {
-                from: path.resolve(__dirname, '../manifest.json'), to: path.resolve(__dirname, '../dist/manifest.json')
-            }]),
-            new ExtractTextPlugin("[name].css"),
+            new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        from: path.resolve(__dirname, "../assets/"),
+                        to: path.resolve(__dirname, "../dist/assets"),
+                    },
+                ],
+            }),
+            new MiniCssExtractPlugin(),
             ...generateHTMLPluginConf(input),
         ],
         module: {
-            rules: [{
-                test: /\.vue$/,
-                loader: 'vue-loader',
-                options: {
-                    extractCSS: true
-                }
-            }, {
-                test: /\.css$/,
-                loader: 'style-loader!css-loader'
-            }, {
-                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-                loader: 'url-loader',
-            }]
+            rules: [
+                {
+                    test: /\.vue$/,
+                    loader: "vue-loader",
+                },
+                {
+                    test: /\.m?js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                            presets: [
+                                ["@babel/preset-env", { targets: "defaults" }],
+                            ],
+                        },
+                    },
+                },
+                {
+                    test: /\.css$/,
+                    use: [MiniCssExtractPlugin.loader, "css-loader"],
+                },
+                {
+                    test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+                    loader: "url-loader",
+                },
+            ],
         },
         output: {
-            filename: '[name].js',
+            filename: "[name].js",
             path: path.resolve(__dirname, `../dist/`),
         },
         optimization: {
@@ -69,53 +91,52 @@ const generateConfig = (input) => {
                     commons: {
                         test: /[\\/]node_modules[\\/]/,
                         name: "vendors",
-                        chunks: "all"
+                        chunks: "all",
                     },
-
                 },
-                name: true
-            }
+                name: false,
+            },
         },
-    }
+    };
 };
 
 module.exports = [
     generateConfig({
-        "background": {
-            input: 'background/index.js',
-            output: 'background/bundle'
+        background: {
+            input: "background/index.js",
+            output: "background/bundle",
         },
-        'recent': {
-            input: 'recent/index.js',
-            output: 'recent/bundle'
+        recent: {
+            input: "recent/index.js",
+            output: "recent/bundle",
         },
-        'landing': {
-            input: 'landing.js',
-            output: 'landing/bundle'
+        landing: {
+            input: "landing.js",
+            output: "landing/bundle",
         },
-        'config': {
-            input: 'config/index.js',
-            output: 'config/bundle'
+        config: {
+            input: "config/index.js",
+            output: "config/bundle",
         },
-        'popups/block': {
-            input: 'popups/block.js',
-            output: 'popups/block/bundle'
+        "popups/block": {
+            input: "popups/block.js",
+            output: "popups/block/bundle",
         },
-        'popups/star': {
-            input: 'popups/star.js',
-            output: 'popups/star/bundle'
+        "popups/star": {
+            input: "popups/star.js",
+            output: "popups/star/bundle",
         },
-        'account/login': {
-            input: 'account/login.js',
-            output: 'account/login/bundle'
-        }, 
-        'account/register': {
-            input: 'account/register.js',
-            output: 'account/register/bundle'
+        "account/login": {
+            input: "account/login.js",
+            output: "account/login/bundle",
         },
-        'subscription': {
-            input: 'subscription/index.js',
-            output: 'subscription/bundle'
-        }
-    })
-]
+        "account/register": {
+            input: "account/register.js",
+            output: "account/register/bundle",
+        },
+        subscription: {
+            input: "subscription/index.js",
+            output: "subscription/bundle",
+        },
+    }),
+];
